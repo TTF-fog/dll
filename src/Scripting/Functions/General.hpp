559c542da750283633eb760ed7ed782062147484
TTF-fog <74908128+TTF-fog@users.noreply.github.com>
2024-11-19 10:01:38 +0530

    Update General.hpp

--- src/Scripting/Functions/General.hpp
+++ src/Scripting/Functions/General.hpp
@@ -1,8 +1,7 @@
 #pragma once
-
 #include <lua.hpp>
 #include "../../Client/GUI/Engine/Engine.hpp"
-
+#include "../../Utils/Utils.cpp"
 namespace General {
     int lua_Notify(lua_State* L) {
         const char* message = luaL_checkstring(L, 1);
@@ -14,6 +13,11 @@ namespace General {
         std::string path = Utils::getRoamingPath() + "\\Flarial";
         lua_pushstring(L, path.c_str());
         return 1;
+    }
+    std::string keyCodeToInt(int* keyCode){
+        std::string key = Utils::getKeyAsString(){
+            
+        }
     }
 
     void registerGeneral(lua_State* L){
